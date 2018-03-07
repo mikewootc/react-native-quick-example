a8cfcba05730aa29d6cf801bb4514d0b9ca84027
Mike Woo <wytabc@126.com>
2018-03-08 05:55:22 +0800

    Add some component demos.

--- src/component/MyUiComponents.js
+++ src/component/MyUiComponents.js
@@ -1,25 +1,101 @@
 import React, { Component } from 'react';
-import{ ScrollView, Image, Text, View, Switch } from 'react-native'
+import{ 
+    StyleSheet, 
+    View, 
+    Text, 
+    Button, 
+    Image, 
+    ScrollView, 
+    Switch,
+    CheckBox,
+    TextInput,
+    Picker,
+} from 'react-native'
 
 export default class MyUiComponents extends Component {
     constructor() {
         super();
         this.state = {
             switchOn: false,
+            checked: false,
+            picked: 'react',
         }
     }
 
     render() {
         return(
-            <ScrollView>
-                <Switch
-                    style={{}}
-                    value={this.state.switchOn}
-                    onValueChange={(value) => this.setState({switchOn: value})}
-                />
+            <ScrollView style={{width: '100%'}}>
+                <View style={[styles.itemLineContainerLayout, styles.itemLineContainer]}>
+                    <Text>Button</Text>
+                    <Button
+                        style={{}}
+                        title="Button"
+                        onPress={() => {}}
+                        color="#4080f0"
+                        accessibilityLabel="Button's accessibilityLabel"
+                    />
+                </View>
+
+
+                <View style={[styles.itemLineContainerLayout, styles.itemLineContainer]}>
+                    <Text>Switch</Text>
+                    <Switch
+                        style={styles.itemLineContainer}
+                        value={this.state.switchOn}
+                        onValueChange={(value) => this.setState({switchOn: value})}
+                    />
+                </View>
+
+                <View style={[styles.itemLineContainerLayout, styles.itemLineContainer]}>
+                    <Text>CheckBox</Text>
+                    <CheckBox
+                        style={{}}
+                        value={this.state.checked}
+                        onValueChange={(value) => this.setState({checked: value})}
+                    />
+                </View>
+
+                <View style={[styles.itemLineContainer]}>
+                    <TextInput
+                        style={{width: '100%'}}
+                        placeholder='TextInput'
+                        onChangeText={(text) => {xxx}}
+                    />
+                </View>
+
+                <View style={styles.itemLineContainer}>
+                    <Picker
+                        selectedValue={this.state.picked}
+                        onValueChange={(value) => this.setState({picked: value})}
+                    >
+                        <Picker.Item label="React xxxxxxxxxxxxxxx" value="react" />
+                        <Picker.Item label="Native ^_^" value="native" />
+                    </Picker>
+                </View>
+
             </ScrollView>
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    itemLineContainerLayout: {
+        flexDirection: 'row', /* row, column */
+        justifyContent: 'space-between',  /* flex-start, center, flex-end, space-around, space-between */
+        alignItems: 'center', /* flex-start, center, flex-end, stretch */
+    },
+    itemLineContainer: {
+        backgroundColor: '#bbccdd',
+        margin: 2,
+        backgroundColor: '#ddeeff',
+        borderWidth: 1,
+        borderColor: '#bbccdd',
+        borderRadius: 5,
+    },
+    red: {
+        color: 'red',
+    },
+});
 
 
